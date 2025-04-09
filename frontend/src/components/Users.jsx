4fa5d4e9e7efcfb2201e76c1a486c56c06e26ee6
Liza Garg <lizagarg5@gmail.com>
2025-04-09 22:24:18 +0530

    before transaction

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {Button} from "./Button";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 export const Users=()=> {
-    const [users, setUsers]=useState([{
-        firstName:"Liza",
-        lastName:"Garg",
-        _id:"1"
-    }]);
+    const [users, setUsers]=useState([]);
+    const [filter, setFilter]=useState("");
+
+    useEffect(()=>{
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter="+filter)
+        .then(response=>{
+            setUsers(response.data.user);
+        })
+    },[filter])       
 
     return <>
         <div className='font-bold mt-6 text-lg'>
             Users
         </div>
         <div className='my-2'>
-            <input type='text' placeholder='Search users' className='w-full px-2 py-1 border rounded border-slate-200' />
+            <input onChange={(e)=>{
+                setFilter(e.target.value);
+            }} type='text' placeholder='Search users' className='w-full px-2 py-1 border rounded border-slate-200' />
         </div>
         <div>
             {users.map(user=><User key={user._id} user={user}></User>)}
@@ -21,6 +29,8 @@ export const Users=()=> {
 }
 
 function User({user}) {
+    const navigate=useNavigate();
+
     return <div className='flex justify-between'>
         <div className='flex'>
             <div className='rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2'>
@@ -31,6 +41,11 @@ function User({user}) {
             <div className='flex flex-col justify-center h-full'>
                 {user.firstName} {user.lastName}
             </div>
+        </div>
+        <div className="flex flex-col justify-center h-ful">
+            <Button onClick={(e)=>{
+                navigate("/send?id="+user._id+"&name="+user.firstName);
+            }}label={"Send Money"} />
         </div>
     </div>
 }
