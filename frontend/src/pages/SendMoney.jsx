ac5923c32c2bb99ed0eab8c1876071e09131134f
Liza Garg <lizagarg5@gmail.com>
2025-04-09 21:07:37 +0530

    signup implemented

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -5,29 +5,29 @@ export const SendMoney = () => {
                 <div className="flex flex-col space-y-1.5 p-6">
                     <h2 className="text-3xl font-bold text-center"></h2>
                 </div>
-                <div class="p-6">
-                <div class="flex items-center space-x-4">
-                    <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                    <span class="text-2xl text-white">A</span>
+                <div className="p-6">
+                <div className="flex items-center space-x-4">
+                    <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
+                    <span className="text-2xl text-white">A</span>
                     </div>
-                    <h3 class="text-2xl font-semibold">Friend's Name</h3>
+                    <h3 className="text-2xl font-semibold">Friend's Name</h3>
                 </div>
-                <div class="space-y-4">
-                    <div class="space-y-2">
+                <div className="space-y-4">
+                    <div className="space-y-2">
                     <label
-                        class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                         for="amount"
                     >
                         Amount (in Rs)
                     </label>
                     <input
                         type="number"
-                        class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
+                        className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                         id="amount"
                         placeholder="Enter amount"
                     />
                     </div>
-                    <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+                    <button className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Initiate Transfer
                     </button>
                 </div>
